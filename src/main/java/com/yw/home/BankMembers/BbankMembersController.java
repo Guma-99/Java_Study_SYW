2114a3d137e3f2961d0854f671bc7e8e91fddb24
Guma-99 <skc06040@naver.com>
2022-08-17 18:02:46 +0900

    0817

--- src/main/java/com/yw/home/BankMembers/BbankMembersController.java
+++ src/main/java/com/yw/home/BankMembers/BbankMembersController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.yw.home.BankMembers.BankMembersService;
 import com.yw.home.BankMembers.BankMembersDTO;
 
+@Controller
+@RequestMapping (value = "/member/*")
 public class BbankMembersController {
 	
 	@Autowired
