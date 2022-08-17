d8cc06cabef3910f95cdeab37c8b8f2df4d2f4f0
songjh11 <jhmelody91@gmail.com>
2022-08-18 00:08:10 +0900

    fd;dfs;

---
+++ src/main/java/com/yw/home/bankaccount/BankAccountController.java
@@ -0,0 +1,37 @@
+package com.yw.home.bankaccount;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping (value="/bankaccount/*")
+public class BankAccountController {
+	
+	@Autowired
+	private BankAccountService service;
+	
+	@RequestMapping (value="add.do", method=RequestMethod.GET)
+	public void setAccount() {
+		System.out.println("AccountGet");
+	}
+	
+	@RequestMapping (value="add.do", method=RequestMethod.POST)
+	public ModelAndView setAccount(BankAccountDTO bankAccountDTO) {
+		System.out.println("AccountPOST");
+		ModelAndView modelAndView = new ModelAndView();
+		int result = service.setAccount(bankAccountDTO);
+		if(result>0) {
+			System.out.println("추가 성공");
+			modelAndView.setViewName("redirect:./list.do");
+			return modelAndView;
+		} else {
+			return modelAndView;
+		}
+	}
+	
+	
+
+}
