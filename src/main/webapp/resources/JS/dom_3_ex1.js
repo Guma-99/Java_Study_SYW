1abad430b23fe72a09832775a92b2fca3571b3e5
Guma-99 <skc06040@naver.com>
2022-08-30 15:42:19 +0900

    0830_operator

--- src/main/webapp/resources/JS/dom_3_ex1.js
+++ src/main/webapp/resources/JS/dom_3_ex1.js
@@ -5,13 +5,32 @@ const d3 = document.getElementById("d3");
 const btn = document.getElementById("btn");
 const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const select = document.getElementById("select");
+const c1 = document.getElementsByClassName("c1");
+
 
 btn.addEventListener("click", function() {
-    console.log("Work");
-    console.log(d1.value);
-    console.log(d2.value);
+    console.log("operator: ", select.value);
 
-    d3.value = d1.value/1 + d2.value/1;
+    if(select.value == "+") {
+        result = d1.value*1 + d1.value*1;
+    } else if(select.value == "-") {
+        result = d1.value*1 - d1.value*1;
+    } else if(select.value == "*") {
+        result = d1.value*1 * d1.value*1;
+    } else if(select.value == "/") {
+        result = d1.value*1 / d1.value*1;
+    } else (select.value == "%")
+        result = d1.value*1 % d1.value*1;
+    
+    
+
+    // console.log("Work");
+    // console.log(d1.value);
+    // console.log(d2.value);
+
+    // d3.value = d1.value/1 + d2.value/1;
 });
 
 
@@ -21,4 +40,16 @@ btn2.addEventListener("click", function(){
 
 btn3.addEventListener("click", function(){
     btn2.click();
+});
+
+//////////////////////////////////////////////////////
+btn4.addEventListener("click", function(){
+    alert(c1);
+   console.log(c1); 
+   
+   for(let i = 0; i < c1.length; i++) {
+    console.log(c1[i].selected);
+   }
+
+   c1[i].selected = true;
 });
