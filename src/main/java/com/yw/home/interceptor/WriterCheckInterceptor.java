50ff17b1f03c757937b7748148407bfc5e967587
Gumaaa <skc06040@naver.com>
2022-09-08 17:55:07 +0900

    0908_interceptor

---
+++ src/main/java/com/yw/home/interceptor/WriterCheckInterceptor.java
@@ -0,0 +1,40 @@
+package com.yw.home.interceptor;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.yw.home.BankMembers.BankMembersDTO;
+import com.yw.home.board.impl.BoardDTO;
+
+public class WriterCheckInterceptor extends HandlerInterceptorAdapter{
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// 로그인 사용자의 ID와 DTO의 작성자가 일치하는가?
+		
+		// 로그인 사용자 정보
+		BankMembersDTO bankMembersDTO = request.getSession().getAttribute();	 
+		
+		// DTO
+		Map<String, Object> map =  modelAndView.getModel();
+		
+		BoardDTO boardDTO = (BoardDTO)map.get("boardDTO");
+		
+		if(!bankMembersDTO.getUserName().equals(boardDTO.getWriter())) {
+			modelAndView.setViewName("common/result");
+			modelAndView.addObject("result", 1);
+			modelAndView.addObject("message", "작성자만 수정이 가능합니다.");
+			modelAndView.addObject("url", "./list");
+		}
+		
+		
+		super.postHandle(request, response, handler, modelAndView);
+	}
+	
+}
